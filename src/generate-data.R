9f4b018e7c47e3f44c4e1e25bcaa2b9f6d9e606b
Matthew Kay <matthew.kay@gmail.com>
2015-05-06 13:53:38 -0700

    factor out analysis from data generation

--- src/generate-data.R
+++ src/generate-data.R
@@ -9,35 +9,42 @@ library(dplyr)
 library(ggplot2)
 library(lme4)
 
-within_subject_sd = 1
-between_subject_sd = 1
+rating_within_subject_sd = 1
+rating_between_subject_sd = 1
 
 #run experiment i with n subjects
-run_experiment = function(i, n) within(list(), {
-    participant_intercept = rnorm(n, 0, between_subject_sd)
+run_experiment = function(i, n) {
+    rating_participant_intercept = rnorm(n, 0, rating_between_subject_sd)
             
-    df = data.frame(interface="control", participant=paste0("p", 1:n), rating=rnorm(n, participant_intercept, within_subject_sd)) %>%
-            rbind(data.frame(interface="treatment", participant=paste0("p", 1:n), rating=rnorm(n, 0.5 + participant_intercept, within_subject_sd)))
+    df = data.frame(
+            interface="control", 
+            participant=paste0("p", 1:n), 
+            rating=rnorm(n, rating_participant_intercept, rating_within_subject_sd)
+        ) %>%
+        rbind(data.frame(
+            interface="treatment", 
+            participant=paste0("p", 1:n), 
+            rating=rnorm(n, 0.5 + rating_participant_intercept, rating_within_subject_sd)
+        ))
     
     df$experiment = factor(paste0("e", i))
-        
-    m = lmer(rating ~ interface + (1|participant), data=df)
-    
-    confint = confint(m, method="Wald")["interfacetreatment",]
-    diff_intervals = data.frame(experiment=paste0("e", i),
-        diff=fixef(m)["interfacetreatment"], 
-        diff_min=confint[1], 
-        diff_max=confint[2],
-        se=summary(experiments[[1]]$m)$coef["interfacetreatment","Std. Error"]
-        )
-})
+    df
+}
 
-experiments = list(
+#all observations from all experiments
+df = rbind(
     run_experiment(1, 20),
     run_experiment(2, 20),
-    run_experiment(3, 20)
+    run_experiment(3, 20),
+    run_experiment(4, 20),
+    run_experiment(5, 20),
+    run_experiment(6, 20),
+    run_experiment(7, 20),
+    run_experiment(8, 20),
+    run_experiment(9, 20),
+    run_experiment(10, 20),
+    run_experiment(11, 20)
 )
 
-df = ldply(experiments, function(e) e$df)
-diff_intervals = ldply(experiments, function(e) e$diff_intervals)
-diffs = ddply(df, ~ experiment + participant, function(df) data.frame(diff=df[2,"rating"] - df[1,"rating"]))
+#participant-level effects (individual differences in ratings)
+participant_effects = ddply(df, ~ experiment + participant, function(df) data.frame(diff=df[2,"rating"] - df[1,"rating"]))
--- src/traditional-analysis.R
+++ src/traditional-analysis.R
@@ -7,18 +7,30 @@ library(metafor)
 
 theme_set(theme_bw())
 
+#calculate study-level effect sizes (mean diff), intervals, and standard error
+study_effects = ddply(df, ~ experiment, function (df) {
+        m = lmer(rating ~ interface + (1|participant), data=df)
+        ci = confint(m, method="Wald")["interfacetreatment",]
+        data.frame(
+            diff = fixef(m)["interfacetreatment"], 
+            diff_min = ci[1],
+            diff_max = ci[2],
+            se = summary(m)$coef["interfacetreatment","Std. Error"]
+        )
+    })
+
 #meta analysis
-mm = rma(yi=diff, sei=se, data=diff_intervals)
+mm = rma(yi=diff, sei=se, data=study_effects)
 smm = summary(mm)
-diff_intervals %<>% rbind(data.frame(
+study_effects %<>% rbind(data.frame(
         experiment="meta", diff = smm$b, diff_min = smm$ci.lb, diff_max = smm$ci.ub, se = smm$se))
 
 #plot of difference
-ggplot(diffs, aes(x=experiment, y=diff)) + 
+ggplot(participant_effects, aes(x=experiment, y=diff)) + 
     geom_hline(yintercept=0, linetype="dashed") +
     geom_hline(yintercept=0.5, linetype="dashed", color="skyblue") +
     geom_point(alpha=0.25, size=3) +
-    geom_pointrange(data=diff_intervals, mapping=aes(ymin=diff_min, ymax=diff_max), color="red", size=0.75) +
+    geom_pointrange(data=study_effects, mapping=aes(ymin=diff_min, ymax=diff_max), color="red", size=0.75) +
     coord_flip()
 
 #save.image("output/e3-1.RData")
